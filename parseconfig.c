0cf1e7a52e1444450b528089d68d83de089637fb
Tim Kuijsten <tim@sans.netsend.nl>
2019-07-31 02:15:45 +0200

    change permission requirements on config file
    Don't allow the group to have write access to the main config file.
    Although this might be used safely, it is not standard Unix practice to
    delegate permissions this way. Furthermore, now that all secrets have
    moved to their own file, it's ok if the group or others have read access
    to the main config file.

--- parseconfig.c
+++ parseconfig.c
@@ -905,9 +905,9 @@ xparseconfigfile(const char *filename, struct cfgifn ***rifnv,
 	if ((fd = open(filename, O_RDONLY|O_CLOEXEC)) == -1)
 		err(1, "%s", filename);
 
-	if (!isfdsafe(fd, 0660))
-		errx(1, "%s: must be owned by the superuser and may only be"
-		    " readable or writable by the owner or group", filename);
+	if (!isfdsafe(fd, 0644))
+		errx(1, "%s: must be owned by the superuser and may not be"
+		    " writable by the group or others", filename);
 
 	xparseconfigfd(fd, rifnv, rifnvsize, unprivuid, unprivgid,
 	    rlogfacilitystr);
